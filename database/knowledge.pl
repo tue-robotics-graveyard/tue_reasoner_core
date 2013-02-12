0e04922017ad7da4471315c83deba142430ebf88
Loy van Beek <loy.vanbeek@gmail.com>
2013-02-12 21:50:54 +0000

    Added the location of a trashbin for the cleanup-challenge
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_reasoner/tue_reasoner_core@12958 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- database/knowledge.pl
+++ database/knowledge.pl
@@ -17,13 +17,14 @@ exploration_target(tue_test_lab, clean_up, living_room, bed_cabinet_1).
 exploration_target(tue_test_lab, clean_up, living_room, cabinet_expedit_1).
 exploration_target(tue_test_lab, clean_up, living_room, bed_1).
 
-base_pose(cabinet_expedit_1, pose_2d(4.952,  1.351,  1.667)).
+base_pose(cabinet_expedit_1, pose_2d(4.952,  1.351,  1.570)).
 base_pose(bed_1,             pose_2d(6.058, -1.598,  3.113)).
 base_pose(bed_cabinet_1,     pose_2d(3.797, -1.240, -1.608)).
 
 region_of_interest(cabinet_expedit_1, point_3d(4.807,  2.102, 1.000)).
 region_of_interest(bed_1,             point_3d(5.009, -1.706, 1.000)).
 region_of_interest(bed_cabinet_1,     point_3d(3.729, -2.286, 1.000)).
+region_of_interest(trashbin,     	  point_3d(5.6,    1.9,   1.1)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                  HELPER RULES
