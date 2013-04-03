a81080089977754bf908ed2f54235c22069d78e8
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-04-03 14:49:54 +0000

    Fixed bug: property_expected now does not return exact(...)
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_reasoner/tue_reasoner_core@14226 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- database/std.pl
+++ database/std.pl
@@ -106,7 +106,8 @@ property_expected(amigo, pose, pose_2d(X, Y, Phi)) :-
 % Returns expected value for property
 property_expected(X, Prop, Val) :-
     property(X, Prop, PDF),
-    get_expected(PDF, Val).
+    get_expected(PDF, Val2),
+    (Val2 = exact(Val) ; Val = Val2).
 
 % Returns expected value of a PDF
 get_expected(gaussian(vector(Val), _), Val).
