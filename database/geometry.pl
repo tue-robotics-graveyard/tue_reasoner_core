61cfb312c4fe4aa98da0547c556be15f0a79d807
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2013-06-13 15:03:10 +0000

    Updated geometry.pl
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/tue_reasoner/tue_reasoner_core@16708 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- database/geometry.pl
+++ database/geometry.pl
@@ -7,14 +7,25 @@
 
 % % % % % % % % % % % % % % % INIT % % % % % % % % % % % % % % % %
 
-:- dynamic base_pose/2.
-:- dynamic base_pose/4.
+% waypoint(Env, Chal, Name, Pose2D) - For environment Env and 
+%     challenge Chal there is a waypoint with Name and Pose2D
+%     example: waypoint(lab, rips, registration_table, pose_2d(1, 0, -0.5))
+:- dynamic waypoint/4.
 
-:- dynamic point_of_interest/2.
 :- dynamic point_of_interest/4.
+
+:- dynamic exploration_target/4.
+
+% state(X, Y) - X has state Y (e.g. state(entrance_door, open) )
+:- dynamic state/2.
+
+% unreachable(X) - X is a waypoint that cannot be reached
+:- dynamic unreachable/1.
 
 :- dynamic environment/1.
 :- dynamic challenge/1.
+
+:- dynamic instance_of/4
 
 challenge(none).
 
@@ -34,9 +45,19 @@ set_environment(Env) :-
 waypoint(Name, Pose) :-
     challenge(Challenge),
     environment(Env),
-    point_of_interest(Env, Challenge, Name, Pose).    
+    waypoint(Env, Challenge, Name, Pose).    
 
 point_of_interest(Name, Point) :-
     challenge(Challenge),
     environment(Env),
     point_of_interest(Env, Challenge, Name, Point).
+
+exploration_target(Room, Target) :-
+    challenge(Challenge),
+    environment(Env),
+    exploration_target(Env, Challenge, Room, Target).
+
+instance_of(Obj, Class) :-
+    challenge(Challenge),
+    environment(Env),
+    instance_of(Env, Challenge, Obj, Class).
